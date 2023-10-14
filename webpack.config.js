edcee6cf5f1e6b2eb45cdcbd5716d9e9d32f522f
sdeass <verity_hm@163.com>
2023-10-14 17:55:22 +0800

    feat:() create new project

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const ELhtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+    entry: './src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[{
+            test: /\.txt$/,use:'raw-loader'
+        }], 
+    },
+    plugins:[new ELhtmlWebpackPlugin({template:'./src/index.html'})],
+    mode:'development'
+}
